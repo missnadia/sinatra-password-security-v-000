c368f08b2a3b8cef218183eb44b2e5f256a6c1ff
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-15 20:57:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < Sinatra::Base
 	post "/signup" do
 		user = User.new(username: params[:username], password: params[:password])
 		user.save ? redirect "/login" :	redirect "/failure"
-		end
 	end
 
 	get "/login" do
